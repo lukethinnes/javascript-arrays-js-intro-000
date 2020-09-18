636c58564c63a4b85007542e749e4a019e32b023
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-18 00:40:56 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray {
+  var myArray = ['2', '3', '4', '5'];
+  myArray ['1', ...myArray];
+}
+
+function destructivelyAddElementToBeginningOfArray{
+  var myArray = ['2', '3', '4', '5'];
+  myArray = ['5', ...myArray];
+}
+
+function addElementToEndOfArray {
+  var myArray = ['1', '2', '3', '4'];
+  myArray [...myArray, '5'];
+}
+
+function destructivelyAddElementToEndOfArray {
+  var myArray = ['1', '2', '3', '4'];
+  myArray = [...myArray, '5'];
+}
