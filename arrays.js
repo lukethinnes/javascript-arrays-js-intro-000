3695efc55ed13fa10a8b329b878f5a1095fe8e96
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-18 00:50:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,30 @@ function addElementToEndOfArray(array, item){
   newArray.push(item);
   return newArray;
 }
+
+function destructivelyAddElementToEndOfArray(array, item){
+  array.push(item);
+  return array;
+}
+
+function accessElementInArray(array, index){
+   return array[index]
+ }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+   array.shift();
+   return array
+ }
+
+function removeElementFromBeginningOfArray(array){
+   return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
